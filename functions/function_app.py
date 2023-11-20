8909aa48145b5bc9698614f39cc4f0d3029b2e7c
Micheal Nestor <michealnestor@outlook.com>
2023-11-20 23:22:53 +0000

    updated data function to expect timestamps with each request

--- functions/function_app.py
+++ functions/function_app.py
@@ -1,5 +1,5 @@
 import azure.functions as func
-import datetime
+from datetime import datetime
 import json
 import logging
 
@@ -12,8 +12,8 @@ def DataFunction(req: func.HttpRequest) -> func.HttpResponse:
     bad_request_response = func.HttpResponse(
         status_code=400, 
         body="DataFunction endpoint expects a json body of the form: " +
-            "{'id':int,temp:int[8-15],'wind':int[15-25]," +
-            "'humid':int[40-70],'CO2':int[500-1500]}"
+            "{'timestamp':str['%Y-%m-%d %H:%M:%S'],'id':int,temp:int[8-15]" +
+            ",'wind':int[15-25],'humid':int[40-70],'CO2':int[500-1500]}"
     )
 
     # this function expects a json body 
@@ -23,13 +23,29 @@ def DataFunction(req: func.HttpRequest) -> func.HttpResponse:
         return bad_request_response
     else:
         try: # will fail on wrong type and missing attribute
+            timestamp = str(req_body.get('timestamp'))
+            # try to format the timestamp correctly
+            datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+            # get the rest of the body
             sensorId = int(req_body.get('id'))
             temp     = int(req_body.get('temp'))
             wind     = int(req_body.get('wind'))
             humid    = int(req_body.get('humid'))
             CO2      = int(req_body.get('CO2'))
         except TypeError:
+            logging.info('Type error')
             return bad_request_response
+        except ValueError:
+            logging.info('value error')
+            return bad_request_response
+        else:
+            # check range requirements
+            if not (
+                8 <= temp and temp <= 15 and
+                15 <= wind and wind <= 25 and
+                40 <= humid and humid <= 70 and
+                500 <= CO2 and CO2 <= 1500 
+            ):  return bad_request_response
 
     # attempt to update the databse with this data
     database_unavailable_response = func.HttpResponse(
