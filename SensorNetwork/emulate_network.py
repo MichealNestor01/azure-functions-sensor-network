4fbe1f9ca47be2276c902f2b0d44ec538b81375b
Micheal Nestor <michealnestor@outlook.com>
2023-11-21 16:22:26 +0000

    Renamed directory

--- SensorNetwork/emulate_network.py
+++ SensorNetwork/emulate_network.py
@@ -1,3 +1,4 @@
+from typing import Any
 from sensor import Sensor
 import requests
 from datetime import datetime, timedelta
@@ -7,13 +8,13 @@ URL = "https://distributed-systems-coursework-data-function.azurewebsites.net/ap
 TOTAL_SENSORS = 20
 # start time is 9am on 15/11/2023, sensors produce data once an hour
 # sensors were active for 10 hours
-START_TIME = datetime(2023, 11, 15, 9)
+START_TIME = datetime(2023, 11, 16, 10)
 INTERVAL = timedelta(minutes=60)
-TOTAL_HOURS = 24
+TOTAL_HOURS = 1
 
 def report_to_data_function(sensor: Sensor, timestamp: datetime) -> None:
     print(f"Sending data from sensor {sensor.id} at {timestamp.strftime('%Y-%m-%d %H:%M:%S')} to DataFunction")
-    sensor_values = sensor.get_sensor_values_as_dict()
+    sensor_values: dict[str, Any] = sensor.get_sensor_values_as_dict()
     sensor_values["id"] = sensor.id
     sensor_values["timestamp"] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
     res = requests.post(URL, json=sensor_values)
