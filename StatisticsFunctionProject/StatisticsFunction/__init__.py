d78d99c8c23f1d662a71eba0fd18c593edf087fa
Micheal Nestor <michealnestor@outlook.com>
2023-11-21 16:22:40 +0000

    Setup the sql trigger

--- StatisticsFunctionProject/StatisticsFunction/__init__.py
+++ StatisticsFunctionProject/StatisticsFunction/__init__.py
@@ -1,24 +1,7 @@
 import logging
+import json
 
-from azure.functions import HttpRequest, HttpResponse
-
-
-def main(req: HttpRequest) -> HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
-        )
+def main(sensorDataChanges) -> None:
+    # see the database change
+    logging.info("SQL Changes: %s\n", json.loads(sensorDataChanges))
+    # read already existing data from the database
